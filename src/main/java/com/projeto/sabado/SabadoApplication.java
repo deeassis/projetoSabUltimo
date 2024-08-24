350027b48afff39b29cf294bafe531d90ef0f452
deeassis <deeassis@icloud.com>
2024-08-24 12:57:06 -0300

    AdicionandoArquivos

---
+++ src/main/java/com/projeto/sabado/SabadoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.sabado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/sabado/SabadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.sabado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
